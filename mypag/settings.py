9c8dab5ee47b83d6ad9cbac330d65cd3bc4c7dd5
Luis Gil <gilluis12@gmail.com>
2019-02-08 09:42:30 -0600

    settings ota vez

--- mypag/settings.py
+++ mypag/settings.py
@@ -120,4 +120,4 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 #STATICFILES_DIRS = (os.path.join(BASE_DIR, 'blog/static'),)
-STATIC_ROOT = os.path.join(BASE_DIR,'blog/static')
+STATIC_ROOT = os.path.join(BASE_DIR,'djangovir/blog/static')
